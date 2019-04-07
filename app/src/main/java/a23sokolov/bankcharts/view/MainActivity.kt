7f312171ac56e68bdddcc31bfd6f2ea50a3e2888
a.v.sokolov <a.v.sokolov@tinkoff.ru>
2019-04-07 20:32:48 +0700

    added editText to write points count

--- app/src/main/java/a23sokolov/bankcharts/view/MainActivity.kt
+++ app/src/main/java/a23sokolov/bankcharts/view/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : BaseActivity<MainViewModel>() {
     private fun initViewModel(viewModel: MainViewModel) {
         afterViewModelInited(viewModel)
         downloadBtn.setOnClickListener { view ->
-            viewModel.getPointsBtnClicked(10)
+            viewModel.getPointsBtnClicked(countPoints.text.toString().toInt())
         }
     }
 
